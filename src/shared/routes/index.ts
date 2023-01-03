54a3a14da0cf3db1ad89831bdfc7b216cd733ca8
Paulo Andreola <pauloandreola@gmail.com>
2023-01-03 20:05:30 -0300

    feat: initial config routes and access

---
+++ src/shared/routes/index.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+// import { TasksRepository } from '../../modules/tasks/infra/repositories/implementations/tasksRepository';
+// import { UsersRepository } from '../../modules/users/infra/repositories/implementations/usersRepository';
+// import { tasksRoutes } from '../../modules/tasks/routes/tasks.routes';
+// import { usersRoutes } from '../../modules/users/routes/users.routes';
+
+export const router = Router();
+
+// const createTableUserRepository = new UsersRepository();
+// createTableUserRepository.createUserTable();
+
+// const createTableTaskRepository = new TasksRepository();
+// createTableTaskRepository.createTaskTable();
+
+// router.use('/tasks', tasksRoutes);
+
+// router.use('/users', usersRoutes);
