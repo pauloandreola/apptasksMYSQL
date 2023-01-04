fedc0397d7ab5989438c25074ea002135b403ff3
Paulo Andreola <pauloandreola@gmail.com>
2023-01-04 08:41:38 -0300

    feat: user and task routes files

---
+++ src/shared/routes/tasks.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+
+// import { createTaskController } from "../useCases/createTask";
+// import { deleteTaskController } from "../useCases/deleteTask";
+// import { listAllTasksController } from '../useCases/listAllTasks';
+// import { listTaskController } from '../useCases/listTask';
+// import { updateTaskController } from "../useCases/updateTask";
+
+export const tasksRoutes = Router();
+
+// tasksRoutes.post('/', (request, response) => createTaskController.handle(request, response));
+
+// tasksRoutes.delete('/:id', (request, response) => deleteTaskController.handle(request, response));
+
+// tasksRoutes.get('/', (request, response) => listAllTasksController.handle(request, response));
+
+// tasksRoutes.get('/:id', (request, response) => listTaskController.handle(request, response));
+
+// tasksRoutes.put('/:id', (request, response) => updateTaskController.handle(request, response));
---
+++ src/shared/routes/user.routes.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+// import multer from 'multer';
+
+// import uploadConfig from '../../../config/uploads';
+
+// import { ensureAuthenticated } from '../../../middlewares/ensureAuthenticated';
+// import { createUserController } from '../useCases/createUser';
+// import { loginUserController } from '../useCases/loginUser';
+// import { updateAvatarUserController } from '../useCases/updateAvatarUser';
+// import { updateUserController } from '../useCases/updateUser';
+
+export const usersRoutes = Router();
+
+// const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
+
+// usersRoutes.post('/register', (request, response) => createUserController.handle(request, response));
+
+// usersRoutes.get('/login', (request, response) => loginUserController.handle(request, response));
+
+// usersRoutes.use(ensureAuthenticated);
+
+// usersRoutes.patch('/avatar', (request, response) => updateAvatarUserController.handle(request, response));
+
+// usersRoutes.patch('/:id', (request, response) => updateUserController.handle(request, response));
