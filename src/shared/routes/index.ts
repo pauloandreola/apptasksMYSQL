f8bbb6043ed84821d51ae5082d53afdcb899c978
Paulo Andreola <pauloandreola@gmail.com>
2023-01-04 23:55:19 -0300

    refactor: coment user and task create table

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -1,16 +1,16 @@
 import { Router } from 'express';
-import { TasksRepository } from '../../modules/task/infra/repositories/implementaions/tasksRepository';
-import { UsersRepository } from '../../modules/user/infra/repositories/implementations/usersRepository';
+// import { TasksRepository } from '../../modules/task/infra/repositories/implementations/tasksRepository';
+// import { UsersRepository } from '../../modules/user/infra/repositories/implementations/usersRepository';
 import { tasksRoutes } from '../routes/tasks.routes';
 import { usersRoutes } from '../routes/user.routes';
 
 export const router = Router();
 
-const createTableUserRepository = new UsersRepository();
-createTableUserRepository.createUserTable();
+// const createTableUserRepository = new UsersRepository();
+// createTableUserRepository.createUserTable();
 
-const createTableTaskRepository = new TasksRepository();
-createTableTaskRepository.createTaskTable();
+// const createTableTaskRepository = new TasksRepository();
+// createTableTaskRepository.createTaskTable();
 
 router.use('/tasks', tasksRoutes);
 
