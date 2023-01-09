a6b2fc6731d5d50537ac1f5c6e452303082028d2
Paulo Andreola <pauloandreola@gmail.com>
2023-01-09 19:34:17 -0300

    refactor: create async method to db connection

--- src/shared/infra/services/db.ts
+++ src/shared/infra/services/db.ts
@@ -1,13 +1,16 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 
 import 'dotenv/config';
 
 const port = process.env.DB_PORT as number | undefined;
 
-export const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: port,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
+export async function connection () {
+  let connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: port,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  });
+  return connection;
+}
