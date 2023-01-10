31b88c72af8123f5cd65fce4c63ee49dabd6b6b5
Paulo Andreola <pauloandreola@gmail.com>
2023-01-10 12:59:35 -0300

    feat: finish all use case create user

--- src/modules/user/useCase/createUser/createUserUseCase.ts
+++ src/modules/user/useCase/createUser/createUserUseCase.ts
@@ -1,4 +1,5 @@
 import { hash } from 'bcryptjs';
+import * as EmailValidator from 'email-validator';
 
 import { AppError } from '../../../../errors/appErrors';
 
@@ -11,13 +12,26 @@ export class CreateUserUseCase {
 
   async execute({name, email, admin, password, confpassword, department }: ICreateUserDTO): Promise<void> {
 
-    const userAlreadyExists = await this.usersRepository.findByEmail(email);
-
-    if (userAlreadyExists) {
-      throw new AppError('User already exists');
+    if (!name || !email || !password || !confpassword || !department) {
+      throw new AppError('Please verify a blank field');
+    }
+    if (name.length < 3 ) {
+      throw new AppError('Name is too short');
+    }
+    if (password.length < 6 ) {
+    throw new AppError('Password is too short');
     }
     if (password != confpassword) {
       throw new AppError('Password is not the same');
+    }
+    if (!EmailValidator.validate(email)) {
+      throw new AppError('Invalid e-mail');
+    }
+   
+    const userExists = await this.usersRepository.findByEmail(email);
+
+    if (userExists) {
+      throw new AppError('User already exists');
     }
     
     const passwordHash = await hash(password, 8);
