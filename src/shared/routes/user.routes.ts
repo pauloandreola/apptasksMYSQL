671cdaf588673c8aaa7021c709b0ac5c5ad82454
Paulo Andreola <pauloandreola@gmail.com>
2023-01-11 10:50:14 -0300

    feat: upload avatar route actived

--- src/shared/routes/user.routes.ts
+++ src/shared/routes/user.routes.ts
@@ -1,24 +1,25 @@
 import { Router } from 'express';
-// import multer from 'multer';
+import multer from 'multer';
 
-// import uploadConfig from '../../../config/uploads';
+import uploadConfig from '../../config/uploads';
 
-// import { ensureAuthenticated } from '../../../middlewares/ensureAuthenticated';
+import { ensureAuthenticated } from '../../middlewares/ensureAuthenticated';
 import { createUserController } from '../../modules/user/useCase/createUser';
 import { loginUserController } from '../../modules/user/useCase/loginUser';
-// import { updateAvatarUserController } from '../useCases/updateAvatarUser';
+import { updateUserAvatarController } from '../../modules/user/useCase/updateUserAvatar';
 import { updateUserController } from '../../modules/user/useCase/updateUser';
 
 export const usersRoutes = Router();
 
-// const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
+const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
 
 usersRoutes.post('/register', (request, response) => createUserController.handle(request, response));
 
 usersRoutes.get('/login', (request, response) => loginUserController.handle(request, response));
 
-// usersRoutes.use(ensureAuthenticated);
+usersRoutes.use(ensureAuthenticated);
 
-// usersRoutes.patch('/avatar', (request, response) => updateAvatarUserController.handle(request, response));
+usersRoutes.post('/avatar', uploadAvatar.single('avatarFile'), (request, response) => updateUserAvatarController.handle(request, response));
 
 usersRoutes.put('/:id', (request, response) => updateUserController.handle(request, response));
+ 
