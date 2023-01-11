2385a62ee62d582ebe2871409e3a86abd1fd32fa
Paulo Andreola <pauloandreola@gmail.com>
2023-01-11 18:33:06 -0300

    feat: authenticator method done

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,44 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+import { AppError } from '../errors/appErrors';
+import { UsersRepository } from '../modules/user/infra/repositories/implementations/usersRepository';
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('Token missing!', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      'e84e9313c94d7c48de1354c3e7f704a9',
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User does not exists!', 401);
+    }
+
+     request.user = {
+       id: user_id,
+     };
+
+    next();
+  } catch {
+    throw new AppError('Invalid token!', 401);
+  }
+}
