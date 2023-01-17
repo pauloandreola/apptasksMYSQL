256393ee2019df71c3d7f98fa9b8eab1ae74f7ff
Paulo Andreola <pauloandreola@gmail.com>
2023-01-17 13:17:05 -0300

    fix: validation user and password method, included user_id

--- src/modules/user/useCase/loginUser/loginUserUseCase.ts
+++ src/modules/user/useCase/loginUser/loginUserUseCase.ts
@@ -2,6 +2,7 @@ import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken'; 
 
 import { AppError } from "../../../../errors/appErrors";
+
 import { IUsersRepository } from "../../infra/repositories/IUsersRepository";
 
 interface IRequest {
@@ -11,6 +12,7 @@ interface IRequest {
 
 interface IResponse {
   user: {
+    user_id: string,
     name: string;
     email: string;
   };
@@ -18,31 +20,31 @@ interface IResponse {
 }
 
 export class LoginUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor( private usersRepository: IUsersRepository ) {}
 
   async execute({ email, password }: IRequest): Promise<IResponse> {
     
-    const user = await this.usersRepository.findByEmail(email);
-    const userPass = bcryptjs.compare(password, user.password);
-
     if (!email || !password) {
       throw new AppError('Please verify a blank field');
     }
+    const user = await this.usersRepository.findByEmail(email);
     if (!user) {
       throw new AppError('Email or password invalid');
     }
+    const userPass = await bcryptjs.compare(password, user.password);
     if(!userPass) {
       throw new AppError('Please insert email ou password correct')
     }
     
-    const token = jwt.sign({id: user.user_id}, process.env.JWT_PASS ?? '', { expiresIn: '1d' });
+    const token = jwt.sign({ subject: user.email }, process.env.JWT_PASS ?? '', { expiresIn: '1d' });
 
     const tokenReturn: IResponse = {
-      token,
       user: {
+        user_id: user.user_id,
         name: user.name,
         email: user.email,
       },
+      token
     };
     return tokenReturn;
   }
