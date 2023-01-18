92dc2e8125bbebfb06b0819125f62bf7699328a2
Paulo Andreola <pauloandreola@gmail.com>
2023-01-18 14:39:04 -0300

    fix: finish adjust update user avatar finish

--- src/shared/routes/user.routes.ts
+++ src/shared/routes/user.routes.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
 
-// import uploadConfig from '../../config/uploads';
 import multerConfig = require('../../config/uploads');
 
 import { ensureAuthenticated } from '../../middlewares/ensureAuthenticated';
@@ -12,18 +11,13 @@ import { updateUserController } from '../../modules/user/useCase/updateUser';
 
 export const usersRoutes = Router();
 
-// const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
-// const uploadAvatar = multer(multerConfig.upload('./tmp/avatar'));
-
 usersRoutes.post('/register', (request, response) => createUserController.handle(request, response));
 
 usersRoutes.get('/login', (request, response) => loginUserController.handle(request, response));
 
 usersRoutes.use(ensureAuthenticated);
 
-// usersRoutes.post('/avatar', ensureAuthenticated, uploadAvatar.single('avatarFile'), (request, response) => updateUserAvatarController.handle(request, response));
 usersRoutes.post('/avatar/:id', multer(multerConfig).single('avatarFile'), (request, response) => updateUserAvatarController.handle(request, response));
-// usersRoutes.post('/avatar/:id', multer(multerConfig).single('avatarFile'), (request, response) => { console.log(request.file); return response.json("ok") });
 
 usersRoutes.put('/:id', (request, response) => updateUserController.handle(request, response));
  
