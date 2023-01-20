919da5e5a96a6770d1a55c35079cea55ee3d0fc7
Paulo Andreola <pauloandreola@gmail.com>
2023-01-20 08:14:21 -0300

    feat: update swagger

--- src/modules/task/infra/repositories/ITasksRepository.ts
+++ src/modules/task/infra/repositories/ITasksRepository.ts
@@ -6,7 +6,10 @@ export interface ITasksRepository {
   createTaskTable(): Promise<void>;
   deleteTask(task_id: string): Promise<void>;
   endTask(task_id: string, task: string): Promise<Task>;
+  findUserAndTaskById(user_id: string, task_id: string): Promise<Task>;
   findTaskById(task_id: string): Promise<Task>;
+  findUserById(user_id: string): Promise<Task>;
+  listAllTasksById(user_id): Promise<Task[]>;
   listAllTasks(): Promise<Task[]>;
   startTask(task_id: string, task: string): Promise<Task>;
   updateProject(task_id: string, project:string): Promise<Task>;
