68e487f58a16d115d2956385805951eee946b7d5
Paulo Andreola <pauloandreola@gmail.com>
2023-01-20 15:17:58 -0300

    feat: length id validator

--- src/modules/task/useCase/createTask/createTaskUseCase.ts
+++ src/modules/task/useCase/createTask/createTaskUseCase.ts
@@ -5,10 +5,14 @@ import { ITasksRepository } from "../../infra/repositories/ITasksRepository";
 export class CreateTaskUseCase {
   constructor(private tasksRepository: ITasksRepository) {}
   async execute({ user_id, project, task  }: ICreateTaskDTO): Promise<void> {
-    
+
+    if (user_id.length != 36  ) {
+      throw new AppError('User is not valid number');
+    }
     if (!project || !task ) {
       throw new AppError('Please verify a blank field');
     }
+    
     await this.tasksRepository.createTask({ user_id, project, task });
   }
 }
