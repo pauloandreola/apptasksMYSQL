c24b4d1cef9f016ee6647197da5927d1301e8f8d
Paulo Andreola <pauloandreola@gmail.com>
2023-01-20 08:15:06 -0300

    delete: authenticate routes test

--- src/shared/routes/authenticate.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import { authenticateUserController } from '../../../src/modules/user/useCase/authenticateUser';
-
-export const authenticateRoutes = Router();
-
-authenticateRoutes.post('/sessions', (request, response) => authenticateUserController.handle(request, response));
