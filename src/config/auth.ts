3cc90a34e6e70c619fed003175ed468abd589bcc
Paulo Andreola <pauloandreola@gmail.com>
2023-01-26 15:38:58 -0300

    feat: update expires in token time

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,7 +1,5 @@
 export default {
-
-  expires_in_token: '15m',
-  expires_in_refresh_token: '30d',
+  expires_in_token: '240m',
+  expires_in_refresh_token: '10d',
   expires_in_refresh_token_days: 30,
-  
 }
