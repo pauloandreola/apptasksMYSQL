6ec9357bccc50889b709d79aa1fa5269874ff2dc
Paulo Andreola <pauloandreola@gmail.com>
2023-01-26 18:21:17 -0300

    feat: insert new user and task validation

--- src/modules/task/useCase/listTask/listTaskUseCase.ts
+++ src/modules/task/useCase/listTask/listTaskUseCase.ts
@@ -5,11 +5,18 @@ import { ITasksRepository } from "../../infra/repositories/ITasksRepository";
 
 export class ListTaskUseCase {
   constructor(private tasksRepository: ITasksRepository) {}
-  async execute(task_id: string): Promise<Task> {
-    const task = await this.tasksRepository.findTaskById(task_id);
-
+  async execute( user_id, task_id ): Promise<Task> {
+    const taskAlreadyExist = await this.tasksRepository.findTaskById( task_id );
+    if (!taskAlreadyExist) {
+      throw new AppError("Task not found")
+    }
+    const userAlreadyExist = await this.tasksRepository.findUserById( user_id );
+    if (!userAlreadyExist) {
+      throw new AppError("User not found")
+    }
+    const task = await this.tasksRepository.findUserAndTaskById( user_id, task_id );
     if (!task) {
-      throw new AppError("Task not found");
+      throw new AppError("Task or user not found");
     }
     return task;
   }
